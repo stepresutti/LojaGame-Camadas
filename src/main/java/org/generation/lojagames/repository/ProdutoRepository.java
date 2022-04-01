cb7819dc20d9660aa0a760615d0de9b76024478e
stepresutti <stephpresutti@gmail.com>
2022-04-01 14:22:51 -0300

    primeiro commit

---
+++ src/main/java/org/generation/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package org.generation.lojagames.repository;
+
+import java.util.List;
+
+import org.generation.lojagames.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+		public List<Produto> findAllByTituloContainingIgnoreCase (String titulo);
+
+}
